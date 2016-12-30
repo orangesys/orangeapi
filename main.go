b1b74e494672fe2f8b1efc0eab99c8f5f67f07de
gavinzhou <gavin.zhou@gmail.com>
2016-12-30 16:35:14 +0000

    fix kong

--- main.go
+++ main.go
@@ -3,87 +3,114 @@ package main
 import (
 	"fmt"
 	"os"
+//        "reflect"
 
+	_ "github.com/orangesys/orangeapi/helm"
+	_ "github.com/orangesys/orangeapi/k8s"
 	"github.com/orangesys/orangeapi/kong"
-	"github.com/orangesys/orangeapi/helm"
-	"github.com/orangesys/orangeapi/k8s"
 	"github.com/orangesys/orangeapi/config"
 )
 
-
-func create_kong_api(config *config.KongConfiguration) {
+func create_kong_api_plugin(config *config.KongConfiguration, name, writepassword string) error {
 	client := kong.NewClient(nil, config)
-        apis, _, _ := client.APIService.List()
-        fmt.Printf("apis is %+v \n", apis)
-	body := &kong.API{
-	    Name: "test02",
-	    UpstreamURL: "http://test02.inside",
-	    RequestHost: "test02.outside",
+	influxdbAPI := &kong.API{
+	    Name: name + "-influxdb",
+	    UpstreamURL: "http://" + name + "-influxdb.default",
+	    RequestHost: name + ".i.orangesys.io",
 	}
-	_, resp, _ := client.APIService.Create(body)
-	fmt.Printf("api resp is %+v \n\n", resp)
-}
-
-func create_kong_consumer(config *config.KongConfiguration) {
-	client := kong.NewClient(nil, config)
-	body := &kong.Consumer{
-	    Username: "test02",
+	_, iresp, err := client.APIService.Create(influxdbAPI)
+	if iresp.StatusCode != 201 {
+		return fmt.Errorf("%s", "can not create influxdb api")
 	}
-	_, resp, _ := client.ConsumerService.Create(body)
-	fmt.Printf("consumer resp is %+v \n\n", resp)
-}
+	if err != nil {
+		return err
+	}
 
-func create_kong_plugin_jwt(config *config.KongConfiguration) {
-	client := kong.NewClient(nil, config)
-	apiName := "test02"
-	generatePlugin := &kong.Plugin{
+	grafanaAPI := &kong.API{
+	    Name: name + "-grafana",
+	    UpstreamURL: "http://" + name + "-grafana.default",
+	    RequestHost: name + ".g.orangesys.io",
+	}
+	_, gresp, err := client.APIService.Create(grafanaAPI)
+	if gresp.StatusCode != 201 {
+		return fmt.Errorf("%s", "can not create grafana api")
+	}
+	if err != nil {
+		return err
+	}
+
+	apiName := name + "-influxdb"
+	JWTPlugin := &kong.Plugin{
 	    Name: "jwt",
 	}
-	plugin, resp, _ := client.PluginService.Create(generatePlugin, apiName)
-	fmt.Printf("plugin is %+v \n\n resp is %+v \n\n\n ", plugin, resp)
-}
-
-func config_kong_plugin_jwt(config *config.KongConfiguration) {
-	client := kong.NewClient(nil, config)
-	consumerName := "test02"
-	_k, _ := kong.UUID()
-	_s, _ := kong.UUID()
-	generateConfig := &kong.JWTCredential{
-		Key: _k,
-		Secret: _s,
+	_, jwtresp, err := client.PluginService.Create(JWTPlugin, apiName)
+	if jwtresp.StatusCode != 201 {
+		return fmt.Errorf("%s %s", "can not create jwt plugin with api", apiName)
 	}
-	credential, resp, err := client.JWTService.Create(consumerName, generateConfig)
-	fmt.Printf("Create JWT credential :\n%v\n%v\n%v\n%v\n", generateConfig, credential, resp, err)
-}
+	if err != nil {
+		return err
+	}
 
-func config_kong_plugin_correlationid(config *config.KongConfiguration) {
-	client := kong.NewClient(nil, config)
-	apiName := "test02"
-	generatePlugin := &kong.Plugin{
+	correlationIDPlugin := &kong.Plugin{
 	    Name: "correlation-id",
 	    Config: kong.CorrelationIDPluginConfig{
 			HeaderName: "Orangesys-Request-ID",
 			Generator: "tracker",
 	    },
 	}
-	plugin, resp, err := client.PluginService.Create(generatePlugin, apiName)
-	fmt.Printf("Create C-ID plugin :\n%v\n%v\n%v\n%v\n", generatePlugin, plugin, resp, err)
-}
+	_, cidresp, err := client.PluginService.Create(correlationIDPlugin, apiName)
+	if cidresp.StatusCode != 201 {
+		return fmt.Errorf("%s %s", "can not create correlation-id plugin with api", apiName)
+	}
+	if err != nil {
+		return err
+	}
 
-func config_kong_plugin_request_transformer(config *config.KongConfiguration) {
-	client := kong.NewClient(nil, config)
-	apiName := "test02"
-	writepassword := "passwordwrite"
 	querystring := "u:_write,p:" + writepassword
-	generatePlugin := &kong.Plugin{
+	requesttransformerPlugin := &kong.Plugin{
 	    Name: "request-transformer",
 	    Config: kong.RequestTransformerPluginConfig{
 			RemoveQueryString: "jwt",
 			AddQueryString: querystring,
             },
         }
-	plugin, resp, err := client.PluginService.Create(generatePlugin, apiName)
-	fmt.Printf("Create Request-Transformer plugin :\n%v\n%v\n%v\n%v\n", generatePlugin, plugin, resp, err)
+	_, rfresp, err := client.PluginService.Create(requesttransformerPlugin, apiName)
+	if rfresp.StatusCode != 201 {
+		return fmt.Errorf("%s %s", "can not create request-transformer plugin with api", apiName)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func create_kong_consumer_with_jwt(config *config.KongConfiguration, name string) (string, string, error) {
+	client := kong.NewClient(nil, config)
+	generateConsumer := &kong.Consumer{
+	    Username: name,
+	}
+	_, cresp, err := client.ConsumerService.Create(generateConsumer)
+	if cresp.StatusCode != 201 {
+		return "", "", fmt.Errorf("%s %s", "can not create consumer", name)
+	}
+	if err != nil {
+		return "", "", err
+	}
+
+	_k, _ := kong.UUID()
+	_s, _ := kong.UUID()
+	generateConfig := &kong.JWTCredential{
+		Key: _k,
+		Secret: _s,
+	}
+	_, jwtresp, err := client.JWTService.Create(name, generateConfig)
+	if jwtresp.StatusCode != 201 {
+		return "", "", fmt.Errorf("%s %s", "can not create jwt plugin with api", name)
+	}
+	if err != nil {
+		return "", "", err
+	}
+	return _k, _s, nil
 }
 
 func main() {
@@ -92,35 +119,20 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%+v\n", err)
 		os.Exit(1)
 	}
-	create_kong_api(config)
-	create_kong_consumer(config)
-	create_kong_plugin_jwt(config)
-	config_kong_plugin_jwt(config)
-	config_kong_plugin_correlationid(config)
-	config_kong_plugin_request_transformer(config)
+	name := "rlxebz"
+	wp := "mypassword"
 
-//	client := kong.NewClient(nil, config)
-//
-//	api, _, _ := client.APIService.Get("mockbin")
-//	fmt.Printf("API:\n%v\n", api)
-//
-//	plugins, _, _ := client.PluginService.List("mockbin")
-//	fmt.Printf("Plugins:\n%v\n", plugins)
-//
-//	plugin, _, _ := client.PluginService.Get("8e7459c9-0e4e-4307-828b-f27cf7574c77", "mockbin")
-//	fmt.Printf("Plugin:\n%v\n", plugin)
-
-//	enablePlugins, resp, err := client.PluginService.GetEnabledPlugins()
-//	fmt.Printf("Enable Plugins :\n%v\n%v\n%v\n", enablePlugins, resp, err)
-
-//	generatePlugin := &kong.Plugin{
-//		Name: "oauth2",
-//		Config: kong.OAuth2PluginConfig{
-//			EnableClientCredentials: true,
-//		},
-//	}
-//	plugin, resp, err := client.PluginService.CreateOAuth(generatePlugin, "mockbin")
-//	fmt.Printf("Create Plugin :\n%v\n%v\n%v\n%v\n", generatePlugin, plugin, resp, err)
-
-
+	err = create_kong_api_plugin(config, name, wp)
+        if err != nil {
+		fmt.Println(err)
+	//	fmt.Errorf("can not create api with kong", err)
+		os.Exit(1)
+	}
+	key, secret, cerr := create_kong_consumer_with_jwt(config, name)
+	if cerr != nil {
+		fmt.Println(err)
+	//	fmt.Errorf("can not create api with kong", err)
+		os.Exit(1)
+	}
+	fmt.Printf("key is %s, secret is %s\n", key, secret)
 }
