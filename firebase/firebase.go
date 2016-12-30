bdcfee79041feab49b0618444d06f27ca0601eea
gavinzhou <gavin.zhou@gmail.com>
2016-12-30 17:35:10 +0000

    add go jwt

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -1,34 +1,48 @@
-package firebase
+package main
 
 import (
+    "fmt"
+    "os"
+
     "github.com/JustinTulloss/firebase"
     "github.com/orangesys/orangeapi/config"
 )
 
-type Date struct {
-    updatedAt string
-}
-
-func checkuser(endpoint, auth, uuid string) bool {
-    c := firebase.NewClient(endpoint + "/users/" + uuid, auth, nil)
+func CheckUser(uuid string, config *config.FirebaseConfiguration) error {
+    c := firebase.NewClient(config.FirebaseURL + "/users/" + uuid, config.FirebaseAuth, nil)
     var r map[string]interface{}
     err := c.Value(&r)
     if err != nil {
-        return false
+        return err
     }
     if r == nil {
-        return false
+        return fmt.Errorf("%s %s", "can not get consumer", uuid)
     }
-    return true
+    return nil
 }
 
-func savetoken(endpoint, auth, uuid, consumerId, token string) bool {
-    c := firebase.NewClient(endpoint + "/users/" + uuid, auth, nil)
+func SaveToken(uuid, consumerId, token string, config *config.FirebaseConfiguration) error {
+    c := firebase.NewClient(config.FirebaseURL + "/users/" + uuid, config.FirebaseAuth, nil)
     tf := map[string]interface{}{ "consumerID": consumerId, "token": token }
     _, err := c.Set("telegraf", tf, nil)
 
     if err != nil {
-      return false
+	return err
     }
-    return true
+    return nil
+}
+
+func main() {
+    config, _ := config.LoadFirebaseConfig()
+    fmt.Println(config)
+    err := CheckUser("iGzNX6QzfudVlwKtR8CQCj0itIU2", config)
+    if err !=nil {
+        fmt.Println(err)
+	os.Exit(1)
+    }
+    err = SaveToken("iGzNX6QzfudVlwKtR8CQCj0itIU2", "test", "testtest", config)
+    if err !=nil {
+        fmt.Println(err)
+	os.Exit(2)
+    }
 }
