79d067a334cab6a1f894729804ac76473469a3e0
Gavin Zhou <gavin.zhou@gmail.com>
2016-12-30 18:16:00 +0900

    add helm

---
+++ helm/helm.go
@@ -0,0 +1,63 @@
+package helm
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type InfluxdbCommander struct {
+	Name, Retention, Pvcsize string
+}
+
+type GrafanaCommander struct {
+	Name string
+}
+
+func (i *InfluxdbCommander) InstallInfluxdb() error {
+  releasename := "--name=" + i.Name + "-i"
+  opt := "retentionPolicy=" + i.Retention + ",persistence.size=" + i.Pvcsize
+  cmdName := "helm"
+	cmdArgs := []string{
+    "install",
+    releasename,
+    "influxdb",
+    "--set",
+    opt,
+  }
+	return exec.Command(cmdName, cmdArgs...).Run()
+}
+
+func (g *GrafanaCommander) InstallGrafana() error {
+  releasename := "--name=" + g.Name + "-g"
+  cmdName := "helm"
+  cmdArgs := []string{
+    "install",
+    releasename,
+    "grafana",
+  }
+  return exec.Command(cmdName, cmdArgs...).Run()
+}
+
+//func main()  {
+//  name := "rlxebz"
+//  var err error
+//
+//  influxdb := InfluxdbCommander{
+//    Name: name,
+//    Retention: "10d",
+//    Pvcsize: "10Gi",
+//  }
+//  err = influxdb.InstallInfluxdb()
+//  if err != nil {
+//    fmt.Printf("can not create influxdb%+v", err)
+//    os.Exit(98)
+//  }
+//
+//  grafana := GrafanaCommander{ Name: name }
+//  err = grafana.InstallGrafana()
+//  if err != nil {
+//    fmt.Printf("can not create grafana%+v", err)
+//    os.Exit(99)
+//  }
+//}
