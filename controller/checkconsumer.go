ebaae0758c87f001e55dea1a1a8985eb89b05f69
Gavin Zhou <gavin.zhou@gmail.com>
2016-12-31 22:38:31 +0900

    fix format

--- controller/checkconsumer.go
+++ controller/checkconsumer.go
@@ -1,27 +1,26 @@
 package controller
 
 import (
-	_ "fmt"
+		_ "fmt"
 
-	"github.com/orangesys/orangeapi/firebase"
-	"github.com/orangesys/orangeapi/config"
+		"github.com/orangesys/orangeapi/firebase"
+		"github.com/orangesys/orangeapi/config"
 )
 
 func CheckConsumer(uuid string) error {
 //	uuid := "iGzNX6QzfudVlwKtR8CQCj0itIU2"
 //	load env ORANGEAPI_FIREBASE_AUTH
 //	     env ORANGEAPI_FIREBASE_URL
-        firebaseconfig, err := config.LoadFirebaseConfig()
-	if err != nil {
-            return err
-        }
-
-        user := firebase.FirebaseConfiguration{
-	    Config: firebaseconfig,
-	    UUID: uuid,
-        }
-        if err = user.CheckUser(); err !=nil {
-            return err
-        }
-	return nil
+	  firebaseconfig, err := config.LoadFirebaseConfig()
+		if err != nil {
+		    return err
+    }
+    user := firebase.FirebaseConfiguration{
+		    Config: firebaseconfig,
+		    UUID: uuid,
+    }
+    if err = user.CheckUser(); err !=nil {
+        return err
+    }
+		return nil
 }
