b4b94a037a58586b7d39ff5fc7b35bcffb8b979f
Gavin Zhou <gavin.zhou@gmail.com>
2016-12-31 22:54:40 +0900

    check env firebase auth

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	_ "os"
+	"fmt"
+	"os"
 	"log"
 
 	"github.com/orangesys/orangeapi/server"
 	"github.com/orangesys/orangeapi/k8s"
+	"github.com/orangesys/orangeapi/config"
 )
 
 func main() {
+		if firebaseconfig, err := config.LoadFirebaseConfig()
+		if err != nil {
+				fmt.Println(err)
+		}
+		if firebaseconfig.FirebaseAuth == "" {
+				log.Println("cat not get FirebaseAuth")
+				os.Exit(1)
+		}
 		printVersion()
 		log.Println("Starting orangeapi...")
 		k8s.WaitForKubernetesProxy()
