e867b7a64d78326e0d8a3bb127a2afc093de6e61
Gavin Zhou <gavin.zhou@gmail.com>
2017-01-01 18:06:14 +0900

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,4 @@ if [ -z "${_tag}" ]; then
 fi
 
 docker tag "orangesys/alpine-orangeapi:${_tag}" "asia.gcr.io/saas-orangesys-io/alpine-orangeapi:${_tag}"
-docker login -e $DOCKER_EMAIL -u _json_key -p "$(cat ${HOME}/account-auth.json)" https://asia.gcr.io
-docker push asia.gcr.io/saas-orangesys-io/alpine-orangeapi:${_tag}
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/saas-orangesys-io/alpine-kong:${tag}
