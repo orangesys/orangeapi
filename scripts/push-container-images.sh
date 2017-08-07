753d2e4387267be3c38f274e9e343bc301d31d86
Gavin Zhou <gavin.zhou@gmail.com>
2017-08-07 22:40:09 +0900

    test ci 2.0

---
+++ scripts/push-container-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+_v=$(git describe --always --tags)
+version=${_v#*v}  
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker push "orangesys/alpine-orangeapi:${version}"
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
+gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
+gcloud config set project $PROJECT_NAME
+gcloud docker -- push asia.gcr.io/saas-orangesys-io/alpine-orangeapi:${version}
+
+docker logout
