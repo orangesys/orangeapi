1fd431201fd7121896dce7eae559cd977849ebab
Gavin Zhou <gavin.zhou@gmail.com>
2017-08-07 23:02:57 +0900

    change docker-enterpoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ helm init --client-only
 if ! helm version; then exit 99;fi
 helm repo remove local
 helm repo remove stable
-helm repo add or-charts https://storage.googleapis.com/orangesys-io-charts
+helm repo add or-charts https://orangesys.github.io/charts
 helm repo update
 
-exec "/orangeapi"
+exec "/orangeapi_linux-amd64"
