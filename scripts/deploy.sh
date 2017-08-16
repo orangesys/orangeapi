fb7a0d130ca3ec9789186ac4bf53da8f8598de45
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 18:02:14 +0900

    fix mistake

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -xe
 version=$(git describe --always --tags|sed 's/^v//')
 
 kubectl --namespace apigateway patch deployment sys-orangeapi \
--p '{"spec":{"template":{"spec":{"containers":[{"name":"sys-orangeapi","image":"asia.gcr.io/$PROJECT_NAME/alpine-orangeapi:'"$version"'"}]}}}}'
+-p '{"spec":{"template":{"spec":{"containers":[{"name":"sys-orangeapi","image":"asia.gcr.io/'"$PROJECT_NAME"'/alpine-orangeapi:'"$version"'"}]}}}}'
