770e3112dacc54e39eb96069d2f545202985a1e5
Gavin Zhou <gavin.zhou@gmail.com>
2017-09-01 00:14:49 +0900

    verup influxdb&grafana

--- pkg/wheel/wheel.go
+++ pkg/wheel/wheel.go
@@ -24,7 +24,7 @@ type CreateGrafana struct {
 // WheelInfluxdb create influxdb with wheel
 func (c *CreateInfluxDB) WheelInfluxdb() error {
 	releaseURL := "http://wheel.kube-system:9855/tiller/v2/releases/" + c.Name + "-i/json"
-	c.ChartURL = "https://github.com/orangesys/charts/raw/master/docs/influxdb-0.1.12.tgz"
+	c.ChartURL = "https://github.com/orangesys/charts/raw/master/docs/influxdb-0.1.13.tgz"
 	c.Values.Raw = "{\"retentionPolicy\":" + c.Retention + ",\"persistence.size\":" + c.PVCSize + "}"
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(c)
@@ -38,7 +38,7 @@ func (c *CreateInfluxDB) WheelInfluxdb() error {
 // WheelGrafana create grafana with wheel
 func (c CreateGrafana) WheelGrafana() error {
 	releaseURL := "http://wheel.kube-system:9855/tiller/v2/releases/" + c.Name + "-g/json"
-	c.ChartURL = "https://github.com/orangesys/charts/raw/master/docs/grafana-0.1.18.tgz"
+	c.ChartURL = "https://github.com/orangesys/charts/raw/master/docs/grafana-0.1.21.tgz"
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(c)
 	_, err := http.Post(releaseURL, "application/json; charset=utf-8", b)
