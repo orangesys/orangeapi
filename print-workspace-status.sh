3a0016c2347c518cbe0b630829fe99a89dcefd30
gavinzhou <gavin.zhou@gmail.com>
2018-01-25 17:12:04 +0900

    test ci

--- print-workspace-status.sh
+++ print-workspace-status.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-GIT_TAG=$(git describe --always --tags || echo $TAG_NAME)
+GIT_TAG=$(git describe --abbrev=0 --tags 2>/dev/null || echo $TAG_NAME)
 
 cat << EOF
 STABLE_BUILD_GIT_TAG ${GIT_TAG-}
